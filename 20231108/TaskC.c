60c0ae65efd679082e7a8ff795b37643ae0c5de7
rrrren12221 <renrate1661@gmail.com>
2024-01-10 16:06:18 +0900

    TaskCとTaskD

--- 20231108/TaskC.c
+++ 20231108/TaskC.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-    
+int main() {
+    int i;
+    int arr[10];
+    srand(time(NULL));
+    for (i = 0; i < 10; i++) {
+        arr[i] = rand();
+    }
+    for (i = 0; i < 10; i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+    printf("Loop end\n");
+    return 0;
 }
